32d696f33782b1d8a3c3339b736ffa8729e172f6
Rodion1102333 <50778474+Rodion1102333@users.noreply.github.com>
2023-06-19 22:04:09 +0300

    Add files via upload

---
+++ database/migrations/2022_11_07_035628_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('gty')->unsigned();
+            $table->double('total');
+            $table->tinyInteger('status')->unsigned()->default(0);
+            $table->double('name');
+            $table->double('email');
+            $table->double('phone');
+            $table->double('address');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
